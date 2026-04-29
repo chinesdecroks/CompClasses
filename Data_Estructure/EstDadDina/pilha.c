4019425ee63cc1a30b58ef847ff52f9d947d1ef3
Lucas <gooldelucas@gmail.com>
2026-04-29 11:34:09 -0300

    Pop done in the structure stack

--- Data_Estructure/EstDadDina/pilha.c
+++ Data_Estructure/EstDadDina/pilha.c
@@ -21,11 +21,36 @@ typedef struct no
 
 Pessoa ler_pessoa();
 void imprime_pessoa(Pessoa p);
+//função para a operação de push (empilhar)
+No* empilhar(No *topo);
+//função para a operação de pop (desempilhar)
+//Nesse caso será utilizado ponteiro de ponteiro pois essa função
+//deve tanto retornar o nó desempilhado quanto retornar o novo valor do topo
+// como uma função só pode retornar um único valor o novo nó para o topo será "retornado"
+//para a função main a partir de uma passagem por referencia, mas como o topo já é um ponteiro
+//então será necessário utilizar ponteiro de ponteiro
+No* desempilhar(No **topo)
+{
+    if (*topo != NULL)
+    {
+        No* remover = *topo;
+        *topo = remover->proximo;
+        return remover;
+    }
+    else 
+        printf("\nPilha vazia!\n");
+    return NULL;
+
+}
+
 
 
 int main(void)
 {
-    No *topo = NULL;
+    //Sempre se realiza operções no topo da pilha 
+    //esse ponteiro topo aponta para o nó que estiver no topo da pilha
+    //no caso é inicializado com NULL já que a pilha não tem nenhum nó
+    No *remover, *topo = NULL;
     int opcao;
 
     do 
@@ -37,10 +62,17 @@ int main(void)
         switch(opcao)
         {
             case 1:
-
+                topo = empilhar(topo);
                 break;
             case 2:
-
+                remover = desempilhar(&topo); 
+                if (remover)
+                {
+                    printf("\nElemento removido com sucesso\n");
+                    imprime_pessoa(remover->p);
+                }
+                else
+                    printf("\nSem no a remover\n");
                 break;
             case 3:
 
@@ -68,4 +100,22 @@ Pessoa ler_pessoa()
 void imprime_pessoa(Pessoa p)
 {
     printf("Nome: %s\nData: %2d/%2d/%4d\n", p.nome, p.data.dia, p.data.mes, p.data.ano);
+}
+
+// Essa função cria um novo nó que recebe as informações de uma pessoa
+// e cria um ponteiro para o topo anterior, e novo criado é retornado como 
+// o topo atual
+No* empilhar(No *topo)
+{
+    No *novo = malloc(sizeof(No));
+
+    if (novo)
+    {
+        novo->p = ler_pessoa();
+        novo->proximo = topo;
+        return novo;
+    }
+    else
+        printf("\nErro ao aloccar memoria\n");
+    return NULL;
 }
